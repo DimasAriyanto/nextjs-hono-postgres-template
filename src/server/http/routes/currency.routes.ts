aaeca52be203e54db6e9db2f21104210b338240e
DimasAriyanto <dimas.ariyanto32@gmail.com>
2026-09-03 10:50:24 +0700

    feat: remove authentication middleware from currency conversion route

--- src/server/http/routes/currency.routes.ts
+++ src/server/http/routes/currency.routes.ts
@@ -1,8 +1,7 @@
 import { Hono } from 'hono';
 import { currencyController } from '../controllers';
-import { auth } from '@/server/http/middlewares/auth';
 import { rateLimit } from '@/server/http/middlewares/rate-limit';
 import { convertCurrencyRequest } from '@/server/http/validators/currency.validator';
 
 export const currencyRoutes = new Hono()
-	.get('/convert', auth, rateLimit({ windowSeconds: 60, max: 30 }), convertCurrencyRequest, currencyController.convert);
+	.get('/convert', rateLimit({ windowSeconds: 60, max: 30 }), convertCurrencyRequest, currencyController.convert);
